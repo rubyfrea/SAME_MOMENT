cb9c202a0d3cf0bfccfb51fbb0cd3f0e57972ea0
rubyfrea <rtian@andrew.cmu.edu>
2017-02-17 05:10:05 -0500

    test

--- webapps/settings.py
+++ webapps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y(8#-xp!$@(^2!8z-!7!j3&@62%ne#botex@+krg=95f9cdkzz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.ec2-54-196-33-134.compute-1.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-54-196-33-134.compute-1.amazonaws.com',]
 
 
 # Application definition
